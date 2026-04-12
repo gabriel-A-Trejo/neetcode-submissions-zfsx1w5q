f0f28a1c4443177bdca235536611c84d8fc61aeb
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2026-04-12 12:14:43 -0500

    Add: duplicate-integer - submission-31

---
+++ Data Structures & Algorithms/duplicate-integer/submission-31.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hasSeen = set()
+
+        for num in nums:
+            if num in hasSeen:
+                return True
+            hasSeen.add(num)
+        return False
